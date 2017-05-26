fe0d6467300dd85ac55aa3142ee9c9415ad64758
ssss1029 <sauravkadavath@gmail.com>
2017-05-26 11:36:59 -0700

    finished routing

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/process', require('./routes/process/index'));
 app.use('/results', require('./routes/results/index'));
-app.use('/results', require('./routes/user'));
+app.use('/user', require('./routes/user/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
