9eac7dbf38abebcafc151cce9d8ae445fbbe77cd
ssss1029 <sauravkadavath@gmail.com>
2017-06-05 15:24:11 -0700

    fixed random module bug:

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/process', require('./routes/process/index'));
 app.use('/results', require('./routes/results/index'));
 app.use('/user', require('./routes/user/index'));
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
